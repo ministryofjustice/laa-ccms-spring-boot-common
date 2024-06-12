54e93d213fd4ad058950434fb21551e96dc1b1f6
farrell-m <michael.farrell@digital.justice.gov.uk>
2024-06-12 16:09:17 +0100

    implement auth context holder, use checkstyle

--- laa-ccms-spring-boot-starters/laa-ccms-spring-boot-starter-auth/src/main/java/uk/gov/laa/ccms/springboot/auth/AuthorizedRole.java
+++ laa-ccms-spring-boot-starters/laa-ccms-spring-boot-starter-auth/src/main/java/uk/gov/laa/ccms/springboot/auth/AuthorizedRole.java
@@ -1,9 +1,9 @@
 package uk.gov.laa.ccms.springboot.auth;
 
 /**
- * Holds a list of URIs available for a given role.
+ * Holds a list of uris available for a given role.
  *
  * @param name the name of the role
- * @param URIs the URIs that are accessible to clients that have this role
+ * @param uris the uris that are accessible to clients that have this role
  */
-public record AuthorizedRole(String name, String[] URIs) { }
+public record AuthorizedRole(String name, String[] uris) {}
