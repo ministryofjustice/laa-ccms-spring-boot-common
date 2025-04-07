bca319fb84b03177c2ca3088381625067e210dbb
Jamie Briggs <jamie.briggs@digital.justice.gov.uk>
2025-04-07 11:00:53 +0100

    CCMSPUI-687 Moved MetricsSecurityConfig

--- laa-ccms-spring-boot-starters/laa-ccms-spring-boot-starter-metrics/src/main/java/uk/gov/laa/ccms/springboot/metrics/config/MetricsSecurityConfig.java
+++ laa-ccms-spring-boot-starters/laa-ccms-spring-boot-starter-metrics/src/main/java/uk/gov/laa/ccms/springboot/metrics/config/MetricsSecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
